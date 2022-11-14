0c29e8041025d8751bf4ff02ca9a20a976043c82
Max Sidorenko <fivemoshr@gmail.com>
2022-11-14 13:00:38 +0200

    Update renderCards.js

--- src/js/render/renderCards.js
+++ src/js/render/renderCards.js
@@ -7,13 +7,30 @@ async function renderCards(data) {
   try {
     const response = await fetchGenres();
     if (response.status !== 200) throw new Error(response.status);
-    if (response.data === undefined) throw new Error(response.data);
+    if (response.data === undefined) {
+      throw new Error(response.data, 'undefined');
+    }
+    console.log(data.results);
     return data.results.reduce(
-      (acc, { poster_path, title, genre_ids, vote_average, name }) => {
+      (
+        acc,
+        {
+          poster_path = 'ss',
+          title = 'sd',
+          genre_ids = 'df',
+          vote_average = '0',
+          name,
+        }
+      ) => {
         const genres = response.data.genres.reduce((acc, el) => {
+          // if (genre_ids === undefined) {
+          //   console.log(response);
+          // }
+          console.log(poster_path, title, genre_ids, vote_average, name);
           if (genre_ids.includes(el['id'])) {
             acc.push(el['name']);
           }
+
           return acc;
         }, []);
 
@@ -21,7 +38,9 @@ async function renderCards(data) {
   <img src='${IMAGES_URL + poster_path}' loading='lazy'/>
   <h3 class="movie-card__name">${title || name}</h3>
   <p class="movie-card__genres">
-    ${genres} <span class="movie-card__ratio">${vote_average.toFixed(1)}</span>
+    ${
+      genres.length === 0 ? "haven't genre" : genres
+    } <span class="movie-card__ratio">${vote_average.toFixed(1)}</span>
   </p>
 </li>`;
         return acc;
@@ -33,7 +52,7 @@ async function renderCards(data) {
   }
 }
 
-async function insertMarkup(refOnContainer, markup) {
+function insertMarkup(refOnContainer, markup) {
   return (refOnContainer.innerHTML = markup);
 }
 
